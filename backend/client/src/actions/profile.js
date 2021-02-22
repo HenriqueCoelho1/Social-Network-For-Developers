0a129a91257bb92e9479b79b2f00eea0cec04534
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2021-02-22 18:48:10 -0300

    Finish Profile Actions

--- backend/client/src/actions/profile.js
+++ backend/client/src/actions/profile.js
@@ -3,6 +3,8 @@ import { setAlert } from './alert'
 
 import {
     GET_PROFILE,
+    GET_PROFILES,
+    GET_REPOS,
     PROFILE_ERROR,
     UPDATE_PROFILE,
     ACCOUNT_DELETED,
@@ -17,6 +19,67 @@ export const getCurrentProfile = () => async dispatch => {
 
         dispatch({
             type: GET_PROFILE,
+            payload: res.data
+        })
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        })
+
+    }
+}
+
+// Get All Profiles 
+
+export const getAllProfiles = () => async dispatch => {
+
+    dispatch({ type: CLEAR_PROFILE })
+    try {
+        const res = await axios.get('/api/profile')
+
+        dispatch({
+            type: GET_PROFILES,
+            payload: res.data
+        })
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        })
+
+    }
+}
+
+// Get Profile by id 
+
+export const getProfileById = userId => async dispatch => {
+
+    try {
+        const res = await axios.get(`/api/profile/user/${userId}`)
+
+        dispatch({
+            type: GET_PROFILES,
+            payload: res.data
+        })
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        })
+
+    }
+}
+
+// Get github repos 
+
+export const getGitHubRepos = username => async dispatch => {
+
+    try {
+        const res = await axios.get(`/api/profile/github/${username}`)
+
+        dispatch({
+            type: GET_REPOS,
             payload: res.data
         })
     } catch (err) {
@@ -188,9 +251,7 @@ export const deleteEducation = id => async dispatch => {
 }
 
 
-//Delete account
-
-
+//Delete account && Profile
 export const deleteAccount = () => async dispatch => {
 
     if (window.confirm('Are you sure do you want delete this account?')) {
