23061d79e8b4dacb9bf9e6d9f2db16f6e9a3e79b
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2021-02-25 01:26:00 -0300

    Adding Comments

--- backend/client/src/components/post/Post.js
+++ backend/client/src/components/post/Post.js
@@ -1,9 +1,11 @@
 import React, { Fragment, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import Spinner from '../layout/Spinner'
 import PostItem from '../posts/PostItem'
 import { getPost } from '../../actions/post'
+import CommentForm from './CommentForm'
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
     useEffect(() => {
@@ -13,7 +15,9 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
     return loading || post === null ? (<Spinner />) :
         (
             <Fragment>
+                <Link to='/posts' className='btn'>Back To Posts</Link>
                 <PostItem post={post} showActions={false} />
+                <CommentForm postId={post._id} />
             </Fragment>)
 }
 
